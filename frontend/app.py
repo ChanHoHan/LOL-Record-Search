80635d126914bff95dee3bee8a1a7f18fe781dd1
chanhohan <chhqm2759@naver.com>
2020-11-27 15:58:58 +0900

    [add] main page(get, post 완료)

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET'])
 def main():
-    return "welcome"
+    if request.method == 'GET':
+        return render_template('home.html')
+
+@app.route("/api", methods = ['POST'])
+def api():
+    if request.method == 'POST':
+        result = request.form
+        print(dict(result))
+        return render_template("record.html", result = dict(result)['Name'])
 
 if __name__ == "__main__":
     app.run()

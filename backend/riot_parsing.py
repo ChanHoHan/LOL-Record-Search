22b7d748f1745d20e6ebe7aed5cd71c73c7f6ca7
leegun <akstp88@gmail.com>
2020-11-30 21:36:31 +0900

    player 클래스 생성

--- backend/riot_parsing.py
+++ backend/riot_parsing.py
@@ -1,14 +1,25 @@
 import requests
 import json
 
+class Player:
+    def __init__(self):
+        self.puuid = ""
+        self.skin_id = ""
+        self.last_round = ""
+        self.placement = ""
+        self.time_eliminated = ""
+        self.gold_left = ""
+        self.traits = []
+        self.units = []
 
 if __name__ == "__main__":
-    word = str(input("검색어 : "))
+    #word = str(input("검색어 : "))
     request_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
     "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
     "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
     "Origin": "https://developer.riotgames.com",
-    "X-Riot-Token": "RGAPI-b7d085d6-5037-4f93-b540-fdf0e59dda24"}
+    "X-Riot-Token": "RGAPI-8d086601-5ec1-420c-9ece-c0c9ab462107"}
+    '''
     summoner_url = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + word
     s_response = requests.get(summoner_url,headers = request_headers)
     s_dict = json.loads(s_response.text)
@@ -22,3 +33,12 @@ if __name__ == "__main__":
         record_url = "https://asia.api.riotgames.com/tft/match/v1/matches/" + match
         r_response = requests.get(record_url,headers = request_headers)
         print(r_response.text)
+    '''
+
+    summoner_url = "https://asia.api.riotgames.com/tft/match/v1/matches/KR_4814588960"
+    s_response = requests.get(summoner_url, headers=request_headers)
+    s_dict = json.loads(s_response.text)
+
+    print(s_dict["info"]["participants"][0]["companion"]["skin_ID"])
+    player= Player()
+    print(type(player))
